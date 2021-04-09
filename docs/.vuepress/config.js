11d00536f186fffb901b384077cee2e3c2cff14f
zhongzihao1996 <1184847189@qq.com>
2021-04-09 18:35:43 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'vue-wiki',
+  description: '我的个人网站',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.jpg' }],
+  ],
+  base: './',
+  markdown: {
+    lineNumbers: false
+  },
+  themeConfig: {
+    nav: [
+      { text: '前端基础', link: '/accumulate/' },
+    ],
+    sidebar: 'auto',
+    sidebarDepth: 2,
+  }
+};
